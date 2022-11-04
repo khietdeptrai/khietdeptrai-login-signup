20ed5930f1b519f508a850619a3c26e32b293387
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-11-04 11:06:21 +0700

    add register form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import RegistrationForm from './components/registrationForm';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <RegistrationForm />
     </div>
   );
 }
