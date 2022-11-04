60321ea9f8ad2c1ab8f2ee5a4c2282c821e9a54e
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-11-04 11:47:15 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React, { useState } from "react";
 import './App.css';
 import { Login } from "./Login";
 import { Register } from "./Register";
 
-
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <Register />
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
