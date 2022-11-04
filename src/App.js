bedf1515993439e1b1d31f58cf1c8b6acb270417
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-11-04 11:34:03 +0700

    Revert "change form ui"
    This reverts commit d4258d80cb08627e50a7eb4172c6f777de6e7f68.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useState } from "react";
 import './App.css';
-import { Login } from "./Login";
-import { Register } from "./Register";
+import Header from './components/header';
+import RegistrationForm from './components/registrationForm';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <Header />
+      <RegistrationForm />
     </div>
   );
 }
