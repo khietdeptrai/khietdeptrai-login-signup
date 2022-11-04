40319e8c57d80786e3be67637f9ed6285f831341
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-11-04 11:39:52 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Login } from "./Login";
+import { Register } from "./Register";
+
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
